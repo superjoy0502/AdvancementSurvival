908a716970dfacbaa62cf080a237acbbd4244d29
SuperJoy <superjoy0502@gmail.com>
2020-12-31 00:46:27 -0700

    v0.0.13 - remove unnecessary codes

--- src/main/java/com/github/superjoy0502/advsurvival/AdvancementSurvival.java
+++ src/main/java/com/github/superjoy0502/advsurvival/AdvancementSurvival.java
@@ -32,34 +32,40 @@ public class AdvancementSurvival extends JavaPlugin implements Listener {
         // Enable our class to check for new players using onPlayerJoin()
         getServer().getPluginManager().registerEvents(this, this);
 
-        onlineplayers = (List<Player>) Bukkit.getOnlinePlayers();
-        for (int i = 0; i < onlineplayers.size(); i++){
-            getLogger().info("for loop: " + i);
-            if (!config.getStringList("players.").contains(String.valueOf(onlineplayers.get(i).getUniqueId()))){
-                getLogger().warning("The config file does not contain the uuid of the "+i+ " player!");
-                config.addDefault("players." + onlineplayers.get(i).getUniqueId(), new ArrayList<String>());
-            } else {
-                getLogger().warning("Creating a new list for player " + i + "!");
-                Iterator<Advancement> advancementIterator = getServer().advancementIterator();
-                ArrayList<Advancement> advancementArrayList = new ArrayList<Advancement>();
-
-                while (advancementIterator.hasNext()){
-                    AdvancementProgress advancementProgress = onlineplayers.get(i).getAdvancementProgress(advancementIterator.next());
-                    if (advancementProgress.isDone()){
-                        advancementArrayList.add(advancementProgress.getAdvancement());
-                    }
-                }
-                config.set("players." + onlineplayers.get(i).getUniqueId(), advancementArrayList);
-                getLogger().warning("Config set.");
-            }
-        }
+//        onlineplayers = (List<Player>) Bukkit.getOnlinePlayers();
+//
+//        for (int i = 0; i < onlineplayers.size(); i++){
+//            getLogger().info("for loop: " + i);
+//            if (!config.getStringList("players").contains(String.valueOf(onlineplayers.get(i).getUniqueId()))){
+//                getLogger().warning("The config file does not contain the uuid of the "+i+ " player!");
+//
+//                config.addDefault("players." + onlineplayers.get(i).getUniqueId(), new ArrayList<String>());
+//            } else {
+//                getLogger().warning("Creating a new list for player " + i + "!");
+//                Iterator<Advancement> advancementIterator = getServer().advancementIterator();
+//                ArrayList<Advancement> advancementArrayList = new ArrayList<Advancement>();
+//
+//                while (advancementIterator.hasNext()){
+//                    AdvancementProgress advancementProgress = onlineplayers.get(i).getAdvancementProgress(advancementIterator.next());
+//                    if (advancementProgress.isDone()){
+//                        advancementArrayList.add(advancementProgress.getAdvancement());
+//                    }
+//                }
+//                config.set("players." + onlineplayers.get(i).getUniqueId(), advancementArrayList);
+//                getLogger().warning("Config set.");
+//            }
+//        }
     }
+
+    // Detect what advancements player has achieved and reassign them on a new list
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
         getLogger().info("A player joined.");
-        if (!config.getStringList("players.").contains(String.valueOf(event.getPlayer().getUniqueId()))){
+        getLogger().info("players:\n"+config.getStringList("players"));
+        if (!config.getStringList("players").contains(String.valueOf(event.getPlayer().getUniqueId()))){
             getLogger().warning("A config for " + event.getPlayer().getName() + " does not exist! Creating one...");
+
             config.addDefault("players." + event.getPlayer().getUniqueId(), new ArrayList<String>());
             saveConfig();
             getLogger().info("Config for " + event.getPlayer().getName() + " successfully created.");
@@ -81,6 +87,8 @@ public class AdvancementSurvival extends JavaPlugin implements Listener {
         config.set("players." + event.getPlayer().getUniqueId(), advancementArrayList);
         getLogger().info("Config for " + event.getPlayer().getName() + " successfully created.");
     }
+
+    // Add advancement to list when achieved
 
     @EventHandler
     public void onAdvancementAchieved(PlayerAdvancementDoneEvent event){
